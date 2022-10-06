97f15d4f35f08ddecc7ffcdcb92a729910ee5889
Yonggang Yu <yonggangyu@ucar.edu>
2022-10-06 13:24:27 -0600

    Start with https://dtcenter.org/sites/default/files/comGSIv3.7_EnKFv1.3.tar.gz

---
+++ env/intel-impi_18_chey.sh
@@ -0,0 +1,6 @@
+source /etc/profile.d/modules.sh
+module purge
+module load cmake/3.9.1
+module load intel/18.0.1 ncarenv/1.2 ncarcompilers/0.4.1 mkl/2018.0.1
+module load netcdf/4.6.1
+module load impi/2018.1.163
